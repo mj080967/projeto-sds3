c6781d513df33d4400625850e3e2de2f20c54960
Mauricio J S Cardoso <mj080967@gmail.com>
2021-05-09 00:18:49 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,58 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
 
+    const [chartData, setChartData] = useState<ChartData>({ //Hook: useState: Manter estado no componente
+            labels: {
+                categories: []
+            },
+            series: [
+                {
+                    name: "% Sucesso",
+                    data: []                   
+                }
+            ]
+    });
+
+    useEffect(() => {       //Hook: useEffect: Executar algo na instanciação ou destruição do componente, observar estado
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSuccess[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1) );
+
+            setChartData ( {
+                labels: {
+                    categories: myLabels
+                },
+                series: [
+                    {
+                        name: "% Success",
+                        data: mySeries                   
+                    }
+                ]
+        });
+            console.log(chartData);
+        });
+    }, []);
+    
     const options = {
         plotOptions: {
             bar: {
@@ -10,7 +61,7 @@ const BarChart = () => {
         },
     };
     
-    const mockData = {
+    /*const mockData = {   //Para testar com dados fixos
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
@@ -20,12 +71,12 @@ const BarChart = () => {
                 data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
             }
         ]
-    };
+    };*/
 
     return (
         <Chart 
-            options={{ ...options, xaxis: mockData.labels}}
-            series={mockData.series}
+            options={{ ...options, xaxis: chartData.labels}}
+            series={chartData.series}
             type="bar"
             height="240"
             
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -26,10 +26,10 @@ const DonutChart = () => {
     }, []);
     
 
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+    /*const mockData = {    //Para testar com dados fixos
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    }*/
     
     const options = {
         legend: {
